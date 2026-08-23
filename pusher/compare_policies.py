d156a9e244bce3785125566aaf0751f33cbaae27
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-08-23 19:11:49 +0300

    normalize privileged info

--- pusher/compare_policies.py
+++ pusher/compare_policies.py
@@ -2,10 +2,12 @@ import numpy as np
 from stable_baselines3 import PPO
 from env import CustomPusherEnv
 from train_univ_policy import PrivilegedObservationWrapper
+import gymnasium as gym
 
 def evaluate_model(model_type: str, num_episodes: int =5, max_steps: int =200, render: int =False) -> list:
     print(f"Evaluating {model_type} Model")
 
+    # env = gym.make("Pusher-v5", render_mode=("human" if render else None))
     env = CustomPusherEnv(render_mode=("human" if render else None))
     env = PrivilegedObservationWrapper(env)
 
@@ -23,6 +25,7 @@ def evaluate_model(model_type: str, num_episodes: int =5, max_steps: int =200, r
         steps = 0
 
         while not done:
+            # action, _ = model.predict(obs, deterministic=True)
             if model_type == "UNIVERSAL":
                 action, _ = model.predict(obs, deterministic=True)
             else:
@@ -43,24 +46,24 @@ def evaluate_model(model_type: str, num_episodes: int =5, max_steps: int =200, r
     return results
 
 if __name__ == "__main__":
-    max_steps = 500
-    num_episodes = 200
-    render = False
-    # num_episodes = 5
-    # render = True
+    max_steps = 100
+    # num_episodes = 200
+    # render = False
+    num_episodes = 5
+    render = True
     print(f"Running stress test comparison ({num_episodes} episodes each)...")
 
-    naive_results = evaluate_model("NAIVE", num_episodes, max_steps, render)
-    naive_avg = np.mean([r['steps'] for r in naive_results])
-    print(f"\n--- Results (Average Steps Survived) ---")
-    print(f"Naive Policy:     {naive_avg:.2f} steps")
-    naive_fails = [r for r in naive_results if r['steps'] < max_steps]
-    print(f"\nNaive Policy failed on {len(naive_fails)} out of {num_episodes} episodes.")
-
-
-    # univ_results = evaluate_model("UNIVERSAL", num_episodes, max_steps, render)
-    # univ_avg = np.mean([r['steps'] for r in univ_results])
+    # naive_results = evaluate_model("NAIVE", num_episodes, max_steps, render)
+    # naive_avg = np.mean([r['steps'] for r in naive_results])
     # print(f"\n--- Results (Average Steps Survived) ---")
-    # print(f"Universal Policy: {univ_avg:.2f} steps")
-    # univ_fails = [r for r in univ_results if r['steps'] < max_steps]
-    # print(f"Universal Policy failed on {len(univ_fails)} out of {num_episodes} episodes.")
+    # print(f"Naive Policy:     {naive_avg:.2f} steps")
+    # naive_fails = [r for r in naive_results if r['steps'] < max_steps]
+    # print(f"\nNaive Policy failed on {len(naive_fails)} out of {num_episodes} episodes.")
+
+
+    univ_results = evaluate_model("UNIVERSAL", num_episodes, max_steps, render)
+    univ_avg = np.mean([r['steps'] for r in univ_results])
+    print(f"\n--- Results (Average Steps Survived) ---")
+    print(f"Universal Policy: {univ_avg:.2f} steps")
+    univ_fails = [r for r in univ_results if r['steps'] < max_steps]
+    print(f"Universal Policy failed on {len(univ_fails)} out of {num_episodes} episodes.")
