31db052de454f4f26d486184006ca3f1a4a55b96
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-09-03 17:51:05 +0300

    custom env boilerplate

---
+++ custom_env/env.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+
+class CustomEvnrionment(gym.Env):
+    def __init__(self): pass
+    def _get_obs_(self): pass
+    def _get_reward_(self): pass
+    def reset(self, seed, options): pass
+    def step(self, action): pass
+    def render(self): pass
+    def close(self): pass
+
+# uv run -m mujoco.viewer
